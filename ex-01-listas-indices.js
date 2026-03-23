c5feb3b406baeb6be0eecc2cc772a8eb0e955bd9
brendha-priscilla-nave <brendha.priscilla@nave.org.br>
2026-03-23 14:18:31 -0300

    atividadeConcluida

--- ex-01-listas-indices.js
+++ ex-01-listas-indices.js
@@ -167,7 +167,7 @@ a fruta "abacaxi". Na variável "resposta03" envie a lista completa com a nova f
 */
 // Operações necessárias e resposta a partir da linha abaixo
 frutas[3] = "abacaxi"
-export const resposta03 = frutas[3]
+export const resposta03 = frutas
 
 /* Questão 4
 Ainda com a lista da questão 1, acrescente a fruta "limão" no final da lista, e
@@ -182,7 +182,6 @@ Para finalizar com o array da questão 1, troque as posições dos itens "laranj
 e "manga", e armazene a lista modificada completa na variável "resposta05".
 */
 // Operações necessárias e resposta a partir da linha abaixo
-//const frutas = ["maçã", "banana", "laranja", "uva", "pêra", "manga"]
 let temp = frutas[2]
 frutas[2]= frutas[5]
 frutas[5]= temp
@@ -194,10 +193,10 @@ Crie um array (também com const ao invés de let) chamado "numeros" com os valo
 número no índice zero multiplicado pelo item no penúltimo índice.
 */
 // Array na linha abaixo
-const numeros = [7, 2, 4, 9, 8, 6, 1]
+const numeros = [7, 2, 4, 9, 8, 6, 1];
 
 // Operações necessárias e resposta a partir da linha abaixo
-export const resposta06 = false
+export const resposta06 = numeros[0]*numeros[numeros.length-2];
 
 /* Questão 7
 Seguindo com a lista criada na questão 7, utilizando os índices de cada item,
@@ -206,7 +205,24 @@ de modo que a lista se torne 1, 7, 2, 4, 9, 8, 6. Armazene a lista na
 variável "resposta07"
 */
 // Operações necessárias e resposta a partir da linha abaixo
-export const resposta07 = false
+
+let numerop0= numeros[0]
+let numerop1= numeros[1]
+let numerop2= numeros[2]
+let numerop3= numeros[3]
+let numerop4= numeros[4]
+let numerop5= numeros[5]
+let numerop6= numeros[6]
+
+numeros[0] = numerop6
+numeros[1] = numerop0
+numeros[2] = numerop1
+numeros[3] = numerop2
+numeros[4] = numerop3
+numeros[5] = numerop4
+numeros[6] = numerop5
+
+export const resposta07 = numeros
 
 /* Questão 8
 Continuando com a lista da questão 7, adicione no final da lista o resultado
@@ -214,7 +230,7 @@ da soma de todos os números anteriores da lista. Retorne a lista completa na
 variável "resultado08". É proibido usar métodos de listas e funções.
 */
 // Operações necessárias e resposta a partir da linha abaixo
-export const resposta08 = false
+export const resposta08 = numeros + "," + (numeros[0]+numeros[1]+numeros[2]+numeros[3]+numeros[4]+numeros[5]+numeros[6])
 
 /* Questão 9
 Ainda com a lista da questão 7, mude:
@@ -224,7 +240,16 @@ Ainda com a lista da questão 7, mude:
 E retorne a lista completa na variável "resultado09"
 */
 // Operações necessárias e resposta a partir da linha abaixo
-export const resposta09 = false
+
+let numerop1= numeros[1]
+let numerop3= numeros[3]
+let numerop5= numeros[5]
+
+numeros[1] = numerop5
+numeros[3] = numerop1
+numeros[5] = numerop3
+
+export const resposta09 = numeros
 
 /* Questão 10
 Finalizando com a lista da questão 7, crie uma nova lista com três itens onde:
@@ -234,6 +259,8 @@ Finalizando com a lista da questão 7, crie uma nova lista com três itens onde:
 */
 // Nova lista abaixo
 
+let novaLista = [numeros[6], numeros[5], numeros[4]]
+
 // Operações necessárias e resposta a partir da linha abaixo
-export const resposta10 = false
+export const resposta10 = novaLista
 

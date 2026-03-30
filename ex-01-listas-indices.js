b1cd8d1662732d034416e58c046ad5ae150ab739
sophia-goes-nave <sophia.goes@nave.org.br>
2026-03-30 13:57:53 -0300

    pronto

--- ex-01-listas-indices.js
+++ ex-01-listas-indices.js
@@ -158,27 +158,31 @@ o último item da lista. Use a propriedade "length" para auxiliar a encontrar
 o índice deste último item.
 */
 // Operações necessárias e resposta a partir da linha abaixo
-export const resposta02 = false
+export const resposta02 = frutas[frutas-1]
 
 /* Questão 3\
 Continuando com o array criado na questão 1, altere o quarto item da lista para
 a fruta "abacaxi". Na variável "resposta03" envie a lista completa com a nova fruta.
 */
 // Operações necessárias e resposta a partir da linha abaixo
-export const resposta03 = false
+frutas[3]="abacaxi";
+export const resposta03 = frutas
 
 /* Questão 4
 Ainda com a lista da questão 1, acrescente a fruta "limão" no final da lista, e
 envie a lista inteira na variável "resposta04".
 */
 // Operações necessárias e resposta a partir da linha abaixo
-export const resposta04 = false
+frutas[frutas.length-1]="limão";
+export const resposta04 = frutas
 
 /* Questão 5
 Para finalizar com o array da questão 1, troque as posições dos itens "laranja"
 e "manga", e armazene a lista modificada completa na variável "resposta05".
 */
 // Operações necessárias e resposta a partir da linha abaixo
+frutas[frutas.length-4]="manga"
+frutas[frutas.length-1]="laranja"
 export const resposta05 = false
 
 /* Questão 6
@@ -189,7 +193,8 @@ número no índice zero multiplicado pelo item no penúltimo índice.
 // Array na linha abaixo
 
 // Operações necessárias e resposta a partir da linha abaixo
-export const resposta06 = false
+const numeros=[7, 2, 4, 9, 8, 6, 1];
+export const resposta06 = numeros[0]*numeros[numeros.length-2]
 
 /* Questão 7
 Seguindo com a lista criada na questão 7, utilizando os índices de cada item,
@@ -198,7 +203,14 @@ de modo que a lista se torne 1, 7, 2, 4, 9, 8, 6. Armazene a lista na
 variável "resposta07"
 */
 // Operações necessárias e resposta a partir da linha abaixo
-export const resposta07 = false
+numeros;[numeros[0],numeros[6]]=[numeros[6],numeros[0]];
+numeros;[numeros[1],numeros[6]]=[numeros[6],numeros[1]];
+numeros;[numeros[2],numeros[6]]=[numeros[6],numeros[2]];
+numeros;[numeros[3],numeros[6]]=[numeros[6],numeros[3]];
+numeros;[numeros[4],numeros[6]]=[numeros[6],numeros[4]];
+numeros;[numeros[5],numeros[6]]=[numeros[6],numeros[5]];
+export const resposta07 = numeros
+
 
 /* Questão 8
 Continuando com a lista da questão 7, adicione no final da lista o resultado
@@ -206,7 +218,12 @@ da soma de todos os números anteriores da lista. Retorne a lista completa na
 variável "resultado08". É proibido usar métodos de listas e funções.
 */
 // Operações necessárias e resposta a partir da linha abaixo
-export const resposta08 = false
+let soma = 0;
+
+for (let i = 0; i < numeros.length; i++) {
+  soma += numeros[i];
+}
+
 
 /* Questão 9
 Ainda com a lista da questão 7, mude:
@@ -216,7 +233,11 @@ Ainda com a lista da questão 7, mude:
 E retorne a lista completa na variável "resultado09"
 */
 // Operações necessárias e resposta a partir da linha abaixo
-export const resposta09 = false
+export const resposta08 = soma;
+numeros;[numeros[1],numeros[4]]=[numeros[4],numeros[1]];
+numeros;[numeros[3],numeros[6]]=[numeros[6],numeros[3]];
+numeros;[numeros[5],numeros[1]]=[numeros[1],numeros[5]];
+export const resposta09 = numeros
 
 /* Questão 10
 Finalizando com a lista da questão 7, crie uma nova lista com três itens onde:
